50cc59359ad96566261349f00b62ad42808ba761
Danielle Bergbower <dkw5011@gmail.com>
2019-09-05 23:07:24 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song
+
+@@count = 0
+@@artists = []
+
+attr_accessor :name, :artists, :genre
+
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@count += 1
+  @@artists << self
+end
+
+def self.count
+  @@count
+end
+  
+def self.artists
+  @@artists
+end
+
+def name
+  @name
+end
+
+def genre
+  @genre
+end
+
+def artist
+  @artist
+end
+
+end
